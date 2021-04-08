f21de8ccddbedfec11b33016c727e02fcb5fdb77
Bernard Scheuter <31876955+BernardScheuter@users.noreply.github.com>
2021-04-08 11:05:20 +0200

    async wait lijkt te werken

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,77 @@ namespace StandSitDesk
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitel = new System.Windows.Forms.Label();
+            this.btnResetTimer = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitel
+            // 
+            this.lblTitel.AutoSize = true;
+            this.lblTitel.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitel.ForeColor = System.Drawing.Color.Black;
+            this.lblTitel.Location = new System.Drawing.Point(13, 13);
+            this.lblTitel.Name = "lblTitel";
+            this.lblTitel.Size = new System.Drawing.Size(278, 23);
+            this.lblTitel.TabIndex = 0;
+            this.lblTitel.Text = "Titel van het programma";
+            // 
+            // btnResetTimer
+            // 
+            this.btnResetTimer.Location = new System.Drawing.Point(17, 86);
+            this.btnResetTimer.Name = "btnResetTimer";
+            this.btnResetTimer.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnResetTimer.Size = new System.Drawing.Size(75, 23);
+            this.btnResetTimer.TabIndex = 1;
+            this.btnResetTimer.Text = "Reset Timer";
+            this.btnResetTimer.UseVisualStyleBackColor = true;
+            this.btnResetTimer.Click += new System.EventHandler(this.btnResetTimer_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(14, 50);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(268, 23);
+            this.lblTimer.TabIndex = 2;
+            this.lblTimer.Text = "resterende tijd voor switch";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(346, 186);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnResetTimer);
+            this.Controls.Add(this.lblTitel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitel;
+        private System.Windows.Forms.Button btnResetTimer;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,15 +6,38 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace StandSitDesk
 {
     public partial class Form1 : Form
     {
+        public System.Timers.Timer mijnTimer;
+        public int interval;
         public Form1()
         {
             InitializeComponent();
+            lblTitel.Text = "StandSitDesk V0.1";
+            WaitforTimerAsync();
+        }
+        public static async Task WaitforTimerAsync()
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("wacht seconden:  10");
+                Task.Delay(10 * 1000).Wait();
+            });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Iets om op te clikken tijdens Await task");
+        }
+        private void btnResetTimer_Click(object sender, EventArgs e)
+        {
+            // code om timer op 0 of whatever te zetten.
+            Console.WriteLine("Timer is geReset!");
         }
     }
 }
